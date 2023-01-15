2e9fac4d2c700d0cb3aa1f6d68a725ade0deac88
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-14 20:01:33 -0800

    Added docs.

--- src/IRanges.js
+++ src/IRanges.js
@@ -3,7 +3,7 @@ import * as utils from "./utils.js";
 import * as df from "./DataFrame.js";
 
 /**
- * An IRanges object is a collection of integer ranges.
+ * An IRanges object is a collection of integer ranges, inspired by the class of the same name from the Bioconductor ecosystem.
  * Each range consists of a start position and a width, and may be associated with arbitrary range-level metadata in a {@linkplain DataFrame}.
  * The IRanges defines methods for the following generics:
  *
@@ -33,6 +33,18 @@ export class IRanges {
      **************************************************************************
      **************************************************************************/
 
+    /**
+     * @param {Array|TypedArray} start - Array of start positions for each range.
+     * This should be coercible into an Int32Array.
+     * @param {Array|TypedArray} width - Array of widths for each range.
+     * This should be coercible into an Int32Array.
+     * @param {Object} [options={}] - Optional parameters.
+     * @param {?Array} [options.names=null] - Array of strings of length equal to `start`, containing names for each range.
+     * Alternatively `null`, in which case the ranges are assumed to be unnamed.
+     * @param {?DataFrame} [options.rangeMetadata=null] - A {@linkplain DataFrame} with number of rows equal to the length of `start`, containing arbitrary per-range annotations.
+     * Alternatively `null`, in which case a zero-column DataFrame is automatically constructed.
+     * @param {Object} [options.metadata={}] - Object containing arbitrary metadata as key-value pairs.
+     */
     constructor(start, width, { names = null, rangeMetadata = null, metadata = {} } = {}) {
         this._start = IRanges.#convertToInt32Array(start);
         IRanges.#checkNonNegative(this._start, "start");
@@ -72,22 +84,37 @@ export class IRanges {
      **************************************************************************
      **************************************************************************/
 
+    /**
+     * @return {Int32Array} Array of integers containing the start position for each range.
+     */
     start() {
         return this._start;
     }
 
+    /**
+     * @return {Int32Array} Array of integers containing the end position (specifically, one-past-the-end) for each range.
+     */
     end() {
         return this._start.map((x, i) => x + this._width[i]);
     }
 
+    /**
+     * @return {Int32Array} Array of integers containing the width of each range.
+     */
     width() {
         return this._width;
     }
 
+    /**
+     * @return {?Array} Array of strings containing the name of each range, or `null` if no names are available.
+     */
     names() {
         return this._rangeMetadata.rowNames();
     }
 
+    /**
+     * @return {DataFrame} A {@linkplain DataFrame} with one row corresponding to each range, containing arbitrary per-range metadata.
+     */
     rangeMetadata() {
         return this._rangeMetadata;
     }
@@ -103,6 +130,11 @@ export class IRanges {
      **************************************************************************
      **************************************************************************/
 
+    /**
+     * @param {Array|TypedArray} value - Array of start positions for each range.
+     * This should have length equal to the number of ranges and be coercible into an Int32Array.
+     * @return {IRanges} A reference to this IRanges object, after setting the start positions to `value`.
+     */
     $setStart(value) {
         let candidate = IRanges.#convertToInt32Array(value);
         if (candidate.length !== generics.LENGTH(this)) {
@@ -110,9 +142,14 @@ export class IRanges {
         }
         IRanges.#checkNonNegative(candidate, "start");
         this._start = candidate;
-        return;
+        return this;
     }
 
+    /**
+     * @param {Array|TypedArray} value - Array of widths for each range.
+     * This should have length equal to the number of ranges and be coercible into an Int32Array.
+     * @return {IRanges} A reference to this IRanges object, after setting the widths to `value`.
+     */
     $setWidth(value) {
         let candidate = IRanges.#convertToInt32Array(value);
         if (candidate.length !== generics.LENGTH(this)) {
@@ -120,25 +157,47 @@ export class IRanges {
         }
         IRanges.#checkNonNegative(candidate, "width");
         this._width = candidate;
-        return;
+        return this;
     }
 
+    /**
+     * @param {?Array} value - Array of strings containing a name for each range.
+     * This should have length equal to the number of ranges.
+     * Alternatively `null`, if no names are present.
+     * @return {IRanges} A reference to this IRanges object, after setting the names to `value`.
+     */
     $setNames(value) {
         this._rangeMetadata.$setRowNames(value);
-        return;
+        return this;
     }
 
+    /**
+     * @param {?DataFrame} value - Arbitrary metadata for each range.
+     * This should have number of rows equal to the number of ranges.
+     * Alternatively `null`, in which case all existing per-range metadata is removed.
+     * @return {IRanges} A reference to this IRanges object, after setting the widths to `value`.
+     */
     $setRangeMetadata(value) {
-        if (!(value instanceof df.DataFrame) || generics.LENGTH(value) !== generics.LENGTH(this)) {
-            throw new Error("'rangeMetadata' should be replaced by a DataFrame with the same number of rows");
+        if (value !== null) {
+            if (!(value instanceof df.DataFrame) || generics.LENGTH(value) !== generics.LENGTH(this)) {
+                throw new Error("'rangeMetadata' should be replaced by a DataFrame with the same number of rows");
+            }
+        } else {
+            let existing = this._rangeMetadata;
+            value = new df.DataFrame({}, { rowNames: existing.rowNames(), numberOfRows: existing.numberOfRows() });
         }
         this._rangeMetadata = value;
-        return;
+        return this;
     }
 
+    /**
+     * @param {Object} value - Object containing the metadata.
+     *
+     * @return {IRanges} Reference to this DataFrame after replacing the metadata.
+     */
     $setMetadata(value) {
         this._metadata = value;
-        return;
+        return this;
     }
 
     /**************************************************************************
