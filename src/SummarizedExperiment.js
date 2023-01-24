b90818b54efe56e3d5fe1e88243229796cf48636
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-23 17:00:42 -0800

    Fleshed out the docs, added setters.

--- src/SummarizedExperiment.js
+++ src/SummarizedExperiment.js
@@ -3,8 +3,41 @@ import * as ann from "./Annotated.js";
 import * as df from "./DataFrame.js";
 import * as utils from "./utils.js";
 
+/**
+ * A SummarizedExperiment contains zero or more assays, consisting of multi-dimensional arrays (usually matrices) of experimental data,
+ * as well as {@linkplain DataFrame}s containing further annotations on the rows or columns of those arrays.
+ * The SummarizedExperiment class defines methods for the following generics:
+ * 
+ * - {@linkcode NUMBER_OF_ROWS}
+ * - {@linkcode NUMBER_OF_COLUMNS}
+ * - {@linkcode SLICE_ROWS}
+ * - {@linkcode SLICE_COLUMNS}
+ * - {@linkcode COMBINE_ROWS}
+ * - {@linkcode COMBINE_COLUMNS}
+ *
+ * @extends Annotated
+ */
 export class SummarizedExperiment extends ann.Annotated {
-    constructor(assays, { assayOrder = null, rowData = null, columnData = null, metadata = null, rowNames = null, columnNames = null } = {}) {
+    /**
+     * @param {Object} assays - Object where keys are the assay names and values are multi-dimensional arrays of experimental data.
+     * All arrays should have the same number of rows and columns.
+     * @param {Object} [options={}] - Optional parameters.
+     * @param {?Array} [options.assayOrder=null] - Array of strings specifying the ordering of the assays.
+     * If non-`null`, this should have the same values as the keys of `assays`.
+     * If `null`, an arbitrary ordering is obtained from `assays`.
+     * @param {?DataFrame} [options.rowData=null] - Data frame of row annotations.
+     * If non-`null`, this should have a number of rows equal to the number of rows in each entry of `assays`.
+     * If `null`, an empty {@linkplain DataFrame} is automatically created.
+     * @param {?DataFrame} [options.columnData=null] - Data frame of column annotations.
+     * If non-`null`, this should have a number of columns equal to the number of columns in each entry of `assays`.
+     * If `null`, an empty {@linkplain DataFrame} is automatically created.
+     * @param {?Array} [options.rowNames=null] - Array of strings of length equal to the number of rows in the `assays`, containing row names.
+     * Alternatively `null`, if no row names are present.
+     * @param {?Array} [options.columnNames=null] - Array of strings of length equal to the number of columns in the `assays`, containing column names.
+     * Alternatively `null`, if no column names are present.
+     * @param {Object} [options.metadata={}] - Object containing arbitrary metadata as key-value pairs.
+     */
+    constructor(assays, { assayOrder = null, rowData = null, columnData = null, rowNames = null, columnNames = null, metadata = {} } = {}) {
         super({ metadata });
 
         // Check the assays.
@@ -164,9 +197,18 @@ export class SummarizedExperiment extends ann.Annotated {
      **************************************************************************
      **************************************************************************/
 
+    /**
+     * @param {string|number} i - Identity of the assay to add, either by name or index.
+     * Numeric `i` should be non-negative and less than the number of columns.
+     * @param {*} value - Multi-dimensional array-like object to set/add as the assay.
+     * @return {SummarizedExperiment} Reference to this SummarizedExperiment with modified assays.
+     * - If `i` is a number, the assay at the specified index is replaced.
+     * - If `i` is a string, any assay with the same name is replaced.
+     *   If no such assay exists, a new assay is appended to the list of assays.
+     */
     $setAssay(i, value) {
-        if (generics.LENGTH(value) != this._numberOfRows) {
-            throw new Error("expected 'value' to have the same length as the number of rows in 'x'");
+        if (generics.NUMBER_OF_ROWS(value) !== this.numberOfRows() || generics.NUMBER_OF_COLUMNS(value) !== this.numberOfColumns()) {
+            throw new Error("expected 'value' to have the same dimensions as this 'SummarizedExperiment'");
         }
 
         if (typeof i == "string") {
@@ -178,6 +220,64 @@ export class SummarizedExperiment extends ann.Annotated {
             this._check_index(i);
             this._assays[this._assayOrder[i]] = value;
         }
+        return this;
+    }
+
+    /**
+     * @param {DataFrame} value - Data frame containing the row annotations.
+     * This should have one row for each row of this SummarizedExperiment.
+     * @return {SummarizedExperiment} Reference to this SummarizedExperiment with modified row data.
+     */
+    $setRowData(value) {
+        if (value instanceof df.DataFrame) {
+            throw new Error("'value' should be a DataFrame");
+        }
+        if (value.numberOfRows() !== this.numberOfRows()) {
+            throw new Error("expected 'value' to have the same number of rows as this 'SummarizedExperiment'");
+        }
+        this._rowData = value;
+        return this;
+    }
+
+    /**
+     * @param {DataFrame} value - Data frame containing the column annotations.
+     * This should have one row for each columns of this SummarizedExperiment.
+     * @return {SummarizedExperiment} Reference to this SummarizedExperiment with modified column data.
+     */
+    $setColumnData(value) {
+        if (value instanceof df.DataFrame) {
+            throw new Error("'value' should be a DataFrame");
+        }
+        if (value.numberOfColumns() !== this.numberOfColumns()) {
+            throw new Error("expected 'value' to have the same number of rows as the number of columns of this 'SummarizedExperiment'");
+        }
+        this._columnData = value;
+        return this;
+    }
+
+    /**
+     * @param {Array} names - Array of strings of length equal to the number of rows in this SummarizedExperiment, containing row names.
+     * Alternatively `null`, to remove all row names.
+     * @return {SummarizedExperiment} Reference to this SummarizedExperiment with modified row names.
+     */
+    $setRowNames(names) {
+        if (names !== null) {
+            utils.checkNamesArray(names, "replacement 'names'", this.numberOfRows(), "'numberOfRows()'");
+        }
+        this._rowNames = names;
+        return this;
+    }
+
+    /**
+     * @param {Array} names - Array of strings of length equal to the number of columns in this SummarizedExperiment, containing column names.
+     * Alternatively `null`, to remove all column names.
+     * @return {SummarizedExperiment} Reference to this SummarizedExperiment with modified column names.
+     */
+    $setColumnNames(names) {
+        if (names !== null) {
+            utils.checkNamesArray(names, "replacement 'names'", this.numberOfColumns(), "'numberOfColumns()'");
+        }
+        this._columnNames = names;
         return this;
     }
 }
