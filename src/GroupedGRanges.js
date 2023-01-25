66c6fb056c693a6db799fdf3961245310e21ff46
Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>
2023-01-25 14:16:19 -0800

    Implemented our own version of a GRangesList. (#5)
    This implementation is called GroupedGRanges, which is a nicer terminology as
    we can talk about groups of genomic ranges as being elements of the
    'GroupedGRanges vector'. Like BioC, we still use a single underlying GRanges,
    which avoids a large overhead per group when we have lots of groups; this comes
    at the cost of a slightly more complicated implementation.

---
+++ src/GroupedGRanges.js
@@ -0,0 +1,285 @@
+import * as vec from "./Vector.js";
+import * as gr from "./GRanges.js";
+import * as utils from "./utils.js";
+import * as generics from "./AllGenerics.js";
+
+/**
+ * A GroupedGRanges object is a collection of groups of genomic ranges, inspired by the `GRangesList` class from the Bioconductor ecosystem.
+ * Each group consists of a {@linkplain GRanges} object of arbitrary length, which is most often used to represent a multi-exon gene.
+ * The GroupedGRanges can be considered a vector of groups, and defines methods for the following generics:
+ *
+ * - {@linkcode LENGTH}
+ * - {@linkcode SLICE}
+ * - {@linkcode COMBINE}
+ * - {@linkcode CLONE}
+ *
+ * Our implementation re-uses Bioconductor's strategy of storing the groups in a single concatenated GRanges.
+ * This improves efficiency for large numbers of small GRanges, especially in placeholder objects where all the GRanges are zero-length.
+ * 
+ * @extends Vector
+ */
+export class GroupedGRanges extends vec.Vector {
+    static #computeStarts(lengths) {
+        let starts = new Int32Array(lengths.length);
+        let last = 0;
+        for (var i = 0; i < lengths.length; i++) {
+            starts[i] = last;
+            last += lengths[i];
+        }
+        return { starts: starts, total: last };
+    }
+
+    /**
+     * @param {Array|GRanges} ranges - An array of {@linkplain GRanges} objects, where each element represents a group of genomic ranges.
+     * All objects should have compatible columns in their {@linkplain Vector#elementMetadata elementMetadata}.
+     * 
+     * Alternatively, a single GRanges containing a concatenation of ranges from all groups.
+     * In this case, `rangeLengths` must be supplied.
+     * @param {Object} [options={}] - Optional parameters.
+     * @param {?(TypedArray|Array)} [rangeLengths=null] - Length of the ranges within each group.
+     * This should be coercible to an Int32Array, contain non-negative values, and have a sum equal to the length of `ranges`.
+     * Only used if `ranges` is a single {@linkplain GRanges} object, where each group's ranges are assumed to form contiguous intervals along `ranges`.
+     * @param {?Array} [options.names=null] - Array of strings of length equal to `start`, containing names for each genomic range.
+     * Alternatively `null`, in which case the ranges are assumed to be unnamed.
+     * @param {?DataFrame} [options.elementMetadata=null] - A {@linkplain DataFrame} with number of rows equal to the length of `start`, containing arbitrary per-range annotations.
+     * Alternatively `null`, in which case a zero-column DataFrame is automatically constructed.
+     * @param {Object} [options.metadata={}] - Object containing arbitrary metadata as key-value pairs.
+     */
+    constructor(ranges, { rangeLengths = null, names = null, elementMetadata = null, metadata = {} } = {}) {
+        if (ranges.constructor == Array) {
+            super(ranges.length, { names, elementMetadata, metadata });
+            rangeLengths = new Int32Array(ranges.length);
+            for (var i = 0; i < rangeLengths.length; i++) {
+                if (!(ranges[i] instanceof gr.GRanges)) {
+                    throw new Error("'ranges' must either be a 'GRanges' or an array of 'GRanges'");
+                }
+                rangeLengths[i] = generics.LENGTH(ranges[i]);
+            }
+            ranges = generics.COMBINE(ranges);
+
+        } else {
+            if (!(ranges instanceof gr.GRanges)) {
+                throw new Error("'ranges' must either be a 'GRanges' or an array of 'GRanges'");
+            }
+            if (rangeLengths == null) {
+                throw new Error("'rangeLengths' must be specified when 'ranges' is a 'GRanges'");
+            }
+            super(rangeLengths.length, { names, elementMetadata, metadata });
+            rangeLengths = utils.convertToInt32Array(rangeLengths);
+            utils.checkNonNegative(rangeLengths);
+        }
+
+        this._ranges = ranges;
+        this._rangeLengths = rangeLengths;
+
+        let accumulated = GroupedGRanges.#computeStarts(rangeLengths);
+        this._rangeStarts = accumulated.starts;
+
+        if (accumulated.total !== generics.LENGTH(ranges)) {
+            throw new Error("sum of 'rangeLengths' must be equal to the length of 'ranges'");
+        }
+    }
+
+    /**************************************************************************
+     **************************************************************************
+     **************************************************************************/
+
+    /**
+     * @return {GRanges} The concatenated set of ranges across all groups. 
+     */
+    ranges() {
+        return this._ranges;
+    }
+
+    /**
+     * @return {Int32Array} The start indices for each group's ranges along the concatenated set of ranges returned by {@linkcode GroupedGRanges#ranges ranges}.
+     */
+    rangeStarts() {
+        return this._rangeStarts;
+    }
+
+    /**
+     * @return {Int32Array} The length of each group's ranges along the concatenated set of ranges returned by {@linkcode GroupedGRanges#ranges ranges}.
+     */
+    rangeLengths() {
+        return this._rangeLengths;
+    }
+
+    /**
+     * @param {number} i - Index of the group of interest.
+     * @param {boolean} [options.allowView=false] - Whether a view can be created in any internal slicing operations.
+     *
+     * @return {GRanges} The genomic ranges for group `i`.
+     */
+    group(i, { allowView = false } = {}) {
+        let s = this._rangeStarts[i];
+        return generics.SLICE(this._ranges, { start: s, end: s + this._rangeLengths[i] }, { allowView });
+    }
+
+    /**
+     * @return {number} Number of groups in this object.
+     */
+    numberOfGroups() {
+        return this._rangeStarts.length;
+    }
+
+    /**************************************************************************
+     **************************************************************************
+     **************************************************************************/
+
+    /**
+     * @param {GRanges} ranges - Genomic ranges of length equal to the concatenated set of ranges returned by {@linkcode GroupedGRanges#ranges ranges}.
+     * @return {GroupedGRanges} A reference to this GroupedGRanges object after modifying the internal ranges.
+     */
+    $setRanges(ranges) {
+        if (!(ranges instanceof gr.GRanges)) {
+            throw new Error("'ranges' must be a 'GRanges'");
+        }
+        if (generics.LENGTH(ranges) !== generics.LENGTH(this._ranges)) {
+            throw utils.formatLengthError("'ranges'", "number of ranges");
+        }
+        this._ranges = ranges;
+        return this;
+    }
+
+    /**************************************************************************
+     **************************************************************************
+     **************************************************************************/
+
+    /**
+     * @param {Object} [options={}] - Optional parameters.
+     * @param {?(Array|Set)} [options.restrictToSeqnames=null] - Array or Set containing the sequence names to use in the index.
+     * If `null`, all available sequence names are used.
+     * @param {?(Array|Set)} [options.restrictToStrand=null] - Array or Set containing the strands to use in the index.
+     * If `null`, all available strands are used.
+     *
+     * @return {GroupedGRangesOverlapIndex} A pre-built index for computing overlaps with other {@linkplain GRanges} instances.
+     */
+    buildOverlapIndex({ restrictToSeqnames = null, restrictToStrand = null } = {}) {
+        return new GroupedGRangesOverlapIndex(
+            this._ranges.buildOverlapIndex({ restrictToSeqnames, restrictToStrand }),
+            generics.LENGTH(this._ranges),
+            this._rangeStarts,
+            this._rangeLengths
+        );
+    }
+
+    /**************************************************************************
+     **************************************************************************
+     **************************************************************************/
+
+    _bioconductor_LENGTH() {
+        return this._rangeStarts.length;
+    }
+
+    _bioconductor_SLICE(output, i, { allowView = false } = {}) {
+        super._bioconductor_SLICE(output, i, { allowView });
+
+        output._rangeLengths = generics.SLICE(this._rangeLengths, i, { allowView });
+        let accumulated = GroupedGRanges.#computeStarts(output._rangeLengths);
+        output._rangeStarts = accumulated.starts;
+
+        if (i.constructor == Object) {
+            // Handle this specially for optimizing allowView = true.
+            let s = this._rangeStarts[i.start];
+            output._ranges = generics.SLICE(this._ranges, { start: s, end: s + accumulated.total }, { allowView });
+        } else {
+            let keep = new Int32Array(accumulated.total);
+
+            let counter = 0;
+            for (const j of i) {
+                let start = this._rangeStarts[j];
+                let end = start + this._rangeLengths[j];
+                for (var k = start; k < end; k++) {
+                    keep[counter] = k;
+                    counter++;
+                }
+            }
+
+            output._ranges = generics.SLICE(this._ranges, keep, { allowView });
+        }
+        return;
+    }
+
+    _bioconductor_COMBINE(output, objects) {
+        super._bioconductor_COMBINE(output, objects);
+
+        output._rangeLengths = generics.COMBINE(objects.map(x => x._rangeLengths));
+        let accumulated = GroupedGRanges.#computeStarts(output._rangeLengths);
+        output._rangeStarts = accumulated.starts;
+        output._ranges = generics.COMBINE(objects.map(x => x._ranges));
+
+        return;
+    }
+
+    _bioconductor_CLONE(output, { deepCopy = true }) {
+        super._bioconductor_CLONE(output, { deepCopy });
+        output._rangeLengths = generics.CLONE(this._rangeLengths, { deepCopy });
+        output._rangeStarts = generics.CLONE(this._rangeStarts, { deepCopy });
+        output._ranges = generics.CLONE(this._ranges, { deepCopy });
+        return;
+    }
+}
+
+/**
+ * Pre-built index for overlapping {@linkplain GroupedGRanges} objects.
+ * This is typically constructed using the {@linkcode GroupedGRanges#buildOverlapIndex GroupedGRanges.buildOverlapIndex} method for a "reference" object,
+ * and can be applied to different query GroupedGRanges or {@linkplain GRanges} to identify overlaps with the reference.
+ *
+ * @hideconstructor
+ */
+export class GroupedGRangesOverlapIndex {
+    constructor(index, fullLength, rangeStarts, rangeLengths) {
+        this._index = index;
+        this._rangeStarts = rangeStarts;
+        this._rangeLengths = rangeLengths;
+
+        let rev_map = new Int32Array(fullLength);
+        for (var i = 0; i < rangeStarts.length; i++) {
+            let start = rangeStarts[i];
+            let end = start + rangeLengths[i];
+            for (var s = start; s < end; s++) {
+                rev_map[s] = i;
+            }
+        }
+        this._reverseMapping = rev_map;
+    }
+
+    /**
+     * @param {GroupedGRanges|GRanges} query - The query object, containing ranges to be overlapped with those in the reference GroupedGRanges (that was used to construct this GroupedGRangesOverlapIndex object).
+     * @param {Object} [options={}] - Optional parameters.
+     * @param {boolean} [options.ignoreStrand=true] - Whether to ignore differences in strandedness between the ranges in `query` and the reference object.
+     *
+     * @return {Array} An array of length equal to the number of ranges or groups in `query`,
+     * where each element is an array containing the indices of the overlapping ranges in the reference {@linkplain GRanges} object.
+     */
+    overlap(query, { ignoreStrand = true } = {}) {
+        let output = new Array(this._rangeStarts.length);
+        let rev_map = this._reverseMapping;
+
+        if (query instanceof GroupedGRanges) {
+            let overlaps = this._index.overlap(query._ranges);
+            for (var i = 0; i < query._rangeStarts.length; i++) {
+                let start = query._rangeStarts[i];
+                let end = start + query._rangeLengths[i];
+
+                let results = new Set;
+                for (var s = start; s < end; s++) {
+                    overlaps[s].forEach(x => results.add(rev_map[x]));
+                }
+                output[i] = Array.from(results);
+            }
+
+        } else {
+            let overlaps = this._index.overlap(query);
+            for (var i = 0; i < overlaps.length; i++) {
+                let results = new Set;
+                overlaps[i].forEach(x => results.add(rev_map[x]));
+                output[i] = Array.from(results);
+            }
+        }
+
+        return output;
+    }
+
+}
---
+++ tests/GroupedGRanges.test.js
@@ -0,0 +1,197 @@
+import * as bioc from "../src/index.js";
+import * as utils from "./utils.js";
+
+export function spawn_GRanges_list(N) {
+    let output = [];
+    for (var i = 0; i < N; i++) {
+        output.push(utils.spawn_random_GRanges(Math.ceil(Math.random() * 10)));
+    }
+    return output;
+}
+
+test("constructing a GroupedGRanges works", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+
+    expect(ggr.numberOfGroups()).toEqual(10);
+    expect(ggr.ranges().seqnames()).toEqual(bioc.COMBINE(grl_raw.map(x => x.seqnames())));
+    expect(ggr.ranges().start()).toEqual(bioc.COMBINE(grl_raw.map(x => x.start())));
+    expect(ggr.ranges().width()).toEqual(bioc.COMBINE(grl_raw.map(x => x.width())));
+
+    expect(ggr.group(0).start()).toEqual(grl_raw[0].start());
+    expect(ggr.group(9).start()).toEqual(grl_raw[9].start());
+
+    // Passes along arguments to the Vector constructor.
+    let ggr2 = new bioc.GroupedGRanges(grl_raw, { 
+        metadata: { foo: 1 }, 
+        names: [ "A", "B", "C", "D", "E", "F", "G", "H", "I", "J" ],
+        elementMetadata: new bioc.DataFrame({ foo: utils.spawn_random_vector(10) }) 
+    });
+
+    expect(ggr2.metadata().foo).toBe(1);
+    expect(ggr2.names().length).toBe(10);
+    expect(ggr2.elementMetadata().column("foo").length).toBe(10);
+
+    // Fails if the array contains non-GRanges.
+    expect(() => new bioc.GroupedGRanges([1,2,3])).toThrow("must either be a 'GRanges'");
+})
+
+test("constructing a GroupedGRanges works with runLengths", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+
+    let grl_len = grl_raw.map(bioc.LENGTH);
+    let grl_com = bioc.COMBINE(grl_raw);
+    let ggr2 = new bioc.GroupedGRanges(grl_com, { rangeLengths: grl_len });
+
+    expect(ggr.rangeStarts()).toEqual(ggr2.rangeStarts());
+    expect(ggr.rangeLengths()).toEqual(ggr2.rangeLengths());
+    expect(ggr.ranges().seqnames()).toEqual(ggr2.ranges().seqnames());
+    expect(ggr.ranges().start()).toEqual(ggr2.ranges().start());
+    expect(ggr.ranges().width()).toEqual(ggr2.ranges().width());
+
+    // Fails for inconsistent lengths.
+    expect(() => new bioc.GroupedGRanges(grl_com)).toThrow("rangeLengths");
+    expect(() => new bioc.GroupedGRanges(grl_com, { rangeLengths: [1,2,3] })).toThrow("rangeLengths");
+    expect(() => new bioc.GroupedGRanges(grl_com, { rangeLengths: [1,1,-1,1,1,1,1,1,1,1] })).toThrow("negative");
+    expect(() => new bioc.GroupedGRanges(grl_com, { rangeLengths: [1,1,1,1,1,1,1,1,1,1] })).toThrow("sum of 'rangeLengths'");
+})
+
+test("setting the inner ranges works as expected", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+
+    let rr = bioc.CLONE(ggr.ranges());
+    let str = new Int8Array(bioc.LENGTH(rr));
+    str.fill(-1);
+    rr.$setStrand(str);
+
+    expect(ggr.ranges().strand()[0]).toEqual(0);
+    ggr.$setRanges(rr);
+    expect(ggr.ranges().strand()[0]).toEqual(-1);
+
+    // Errors
+    expect(() => ggr.$setRanges(1)).toThrow("GRanges");
+    expect(() => ggr.$setRanges(bioc.SLICE(rr, [0,1,2,3]))).toThrow("length");
+})
+
+test("LENGTH generic works as expected", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+    expect(bioc.LENGTH(ggr)).toEqual(10);
+})
+
+test("SLICE generic works as expected with indices", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+
+    let indices = [0, 2, 4, 6, 8];
+    let sliced = bioc.SLICE(ggr, indices);
+    expect(sliced.numberOfGroups()).toEqual(indices.length);
+    for (var i = 0; i < indices.length; i++) {
+        expect(sliced.group(i).start()).toEqual(grl_raw[indices[i]].start());
+    }
+
+    expect(sliced.rangeStarts()[0]).toBe(0);
+    expect(Array.from(sliced.rangeLengths())).toEqual(indices.map(i => bioc.LENGTH(grl_raw[i])));
+})
+
+test("SLICE generic works as expected with a range", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+
+    let start = 5;
+    let end = 10;
+    let sliced = bioc.SLICE(ggr, { start, end });
+    expect(sliced.numberOfGroups()).toEqual(end - start);
+
+    let sliced_lengths = [];
+    for (var i = start; i < end; i++) {
+        expect(sliced.group(i - start).start()).toEqual(grl_raw[i].start());
+        sliced_lengths.push(bioc.LENGTH(grl_raw[i]));
+    }
+
+    expect(sliced.rangeStarts()[0]).toBe(0);
+    expect(Array.from(sliced.rangeLengths())).toEqual(sliced_lengths);
+})
+
+test("COMBINE generic works as expected", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+
+    let grl_raw2 = spawn_GRanges_list(15);
+    let ggr2 = new bioc.GroupedGRanges(grl_raw2);
+
+    let combined = bioc.COMBINE([ggr, ggr2]);
+    expect(combined.ranges().seqnames()).toEqual(bioc.COMBINE([ggr.ranges().seqnames(), ggr2.ranges().seqnames()]));
+    expect(combined.ranges().start()).toEqual(bioc.COMBINE([ggr.ranges().start(), ggr2.ranges().start()]));
+    expect(combined.ranges().end()).toEqual(bioc.COMBINE([ggr.ranges().end(), ggr2.ranges().end()]));
+
+    expect(combined.numberOfGroups()).toEqual(25);
+    expect(combined.group(0).start()).toEqual(ggr.group(0).start());
+    expect(combined.group(10).start()).toEqual(ggr2.group(0).start());
+    expect(combined.group(24).start()).toEqual(ggr2.group(14).start());
+})
+
+test("CLONE generic works as expected", () => {
+    let grl_raw = spawn_GRanges_list(10);
+    let ggr = new bioc.GroupedGRanges(grl_raw);
+
+    // Deep copy.
+    let cloned = bioc.CLONE(ggr);
+    cloned.ranges().start()[0] = 1000;
+    expect(cloned.ranges().start()[0]).toBe(1000);
+    expect(ggr.ranges().start()[0]).not.toBe(1000);
+
+    // Shallow copy.
+    let shallow = bioc.CLONE(ggr, { deepCopy: false });
+    shallow.ranges().start()[0] = 1000;
+    expect(shallow.ranges().start()[0]).toBe(1000);
+    expect(ggr.ranges().start()[0]).toBe(1000);
+})
+
+test("overlaps work as expected", () => {
+    let grl_raw = new bioc.GRanges(["A", "A", "B", "B"], new bioc.IRanges([1, 10, 20, 50], [5, 2, 7, 60]));
+    let ggr = new bioc.GroupedGRanges(grl_raw, { rangeLengths: [2, 2] });
+    let index = ggr.buildOverlapIndex();
+
+    // Overlap with a GRanges.
+    let query_gr = new bioc.GRanges(["A", "A", "A", "B", "B", "B"], new bioc.IRanges([1, 6, 11, 10, 30, 45], [3, 4, 5, 20, 10, 20]));
+    {
+        let olap = index.overlap(query_gr);
+        expect(olap[0]).toEqual([0]);
+        expect(olap[1]).toEqual([]);
+        expect(olap[2]).toEqual([0]);
+
+        expect(olap[3]).toEqual([1]);
+        expect(olap[4]).toEqual([]);
+        expect(olap[5]).toEqual([1]);
+
+        // Redundant hits are uniquified.
+        let full = new bioc.GRanges(["A", "B"], new bioc.IRanges([1, 1], [1000, 1000]));
+        let olap2 = index.overlap(full);
+        expect(olap2[0]).toEqual([0]);
+        expect(olap2[1]).toEqual([1]);
+    }
+
+    // Overlap with a GRangesList.
+    {
+        let query_grl1 = new bioc.GroupedGRanges(query_gr, { rangeLengths: [ 3, 3 ] });
+        let olap1 = index.overlap(query_grl1);
+        expect(olap1[0]).toEqual([0]);
+        expect(olap1[1]).toEqual([1]);
+
+        let query_grl2 = new bioc.GroupedGRanges(query_gr, { rangeLengths: [ 4, 2 ] });
+        let olap2 = index.overlap(query_grl2);
+        expect(olap2[0]).toEqual([0, 1]);
+        expect(olap2[1]).toEqual([1]);
+
+        let query_grl3 = new bioc.GroupedGRanges(query_gr, { rangeLengths: [ 1, 1, 2, 1, 1 ] });
+        let olap3 = index.overlap(query_grl3);
+        expect(olap3[0]).toEqual([0]);
+        expect(olap3[1]).toEqual([]);
+        expect(olap3[2]).toEqual([0, 1]);
+        expect(olap3[3]).toEqual([]);
+        expect(olap3[4]).toEqual([1]);
+    }
+})
